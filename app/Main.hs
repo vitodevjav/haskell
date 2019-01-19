aa5d0fec086c544c55e5a1dcbc1672261dc2a734
Vitaly Kazakevich <qulix.vitalik.kazakevich@gmail.com>
2019-01-19 15:11:58 +0300

    HAckerRank compare triplets

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,31 @@
+{-# LANGUAGE DuplicateRecordFields, FlexibleInstances, UndecidableInstances #-}
+
 module Main where
 
-import Data.String.Strip
-import FirstLesson
-import FunctionTemplates
-import FindTriangle
-import WhereAndLet
-import QuickSort
-import PartialApply
-import KollatzRow
-import Lambda
-import Shape
-import MyList
+import Control.Monad
+import Data.Bits
+import Data.List
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
 
-main :: IO ()
-main =
-  let a = Cons 2 $ Cons 1 Empty; b = Cons 3 $ Cons 5 Empty in
-  print $ a ^++ b
+compareAB :: (Ord a) => (Int, Int) -> (a, a) -> (Int, Int)
+compareAB (accA, accB) (a,b) =
+  if a > b then (accA + 1, accB)
+  else if a < b then (accA, accB + 1)
+  else (accA, accB)
+
+compareTriplets :: [Int] -> [Int] -> [Int]
+compareTriplets a b =
+  let tuple = foldl compareAB (0,0) $ zip a b in
+  [fst tuple, snd tuple]
+
+main :: IO()
+main = do
+    let a = [1,2,3]
+        b = [2,5,6]
+
+    print $ compareTriplets a b
 
 
